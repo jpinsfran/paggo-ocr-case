ed88bfe421dd22b7954a95c59644bc9f44c98d42
João Pedro Insfran <jpinsfran@gmail.com>
2025-05-16 19:10:58 -0300

    Primeira Versão

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
       'https://paggo-ocr-case-tau.vercel.app/'
     ],
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
 
